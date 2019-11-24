52cb441372d56ded02a31cbca934aeb8087d9fa8
smokyfog <1473092739@qq.com>
2019-11-24 14:23:21 +0800

    fix

--- mock/user.js
+++ mock/user.js
@@ -1,26 +1,35 @@
 
 const tokens = {
-  admin: {
-    token: 'admin-token'
-  },
-  editor: {
-    token: 'editor-token'
+  yq: {
+    token: 'yq-token'
   }
+  // admin: {
+  //   token: 'admin-token'
+  // },
+  // editor: {
+  //   token: 'editor-token'
+  // }
 }
 
 const users = {
-  'admin-token': {
+  'yq-token': {
     roles: ['admin'],
     introduction: 'I am a super administrator',
     avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
-    name: 'Super Admin'
-  },
-  'editor-token': {
-    roles: ['editor'],
-    introduction: 'I am an editor',
-    avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
-    name: 'Normal Editor'
+    name: '闫强'
   }
+  // 'admin-token': {
+  //   roles: ['admin'],
+  //   introduction: 'I am a super administrator',
+  //   avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+  //   name: 'Super Admin'
+  // },
+  // 'editor-token': {
+  //   roles: ['editor'],
+  //   introduction: 'I am an editor',
+  //   avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+  //   name: 'Normal Editor'
+  // }
 }
 
 export default [
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -43,10 +43,10 @@
 
       <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
 
-      <div class="tips">
+      <!-- <div class="tips">
         <span style="margin-right:20px;">username: admin</span>
         <span> password: any</span>
-      </div>
+      </div> -->
 
     </el-form>
   </div>
@@ -66,16 +66,16 @@ export default {
       }
     }
     const validatePassword = (rule, value, callback) => {
-      if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
+      if (value !== 'yqzsa666') {
+        callback(new Error('密码错误'))
       } else {
         callback()
       }
     }
     return {
       loginForm: {
-        username: 'admin',
-        password: '111111'
+        username: '',
+        password: ''
       },
       loginRules: {
         username: [{ required: true, trigger: 'blur', validator: validateUsername }],
